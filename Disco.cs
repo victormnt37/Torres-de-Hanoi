6068fced6f4cca96a53dec337be7f625e8eafc86
victormnt37 <victormorantf@gmail.com>
2024-03-04 18:59:29 +0100

    inital commit

---
+++ Disco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    public class Disco
+    {
+        // Propiedad para almacenar el tamaño del disco
+        public int Tamaño { get; private set; }
+
+        // Constructor que acepta el tamaño del disco como parámetro
+        public Disco(int tamaño)
+        {
+            Tamaño = tamaño;
+        }
+
+        // Método para imprimir información sobre el disco
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Disco de tamaño {Tamaño}");
+        }
+    }
+}
